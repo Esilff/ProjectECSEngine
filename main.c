5912f14e6841040c8028749ec947b08589790a7d
Banari Veaceslav <veaceslav.banari@adventori.com>
2023-07-04 13:53:49 +0200

    end of basic cmake structure (repair include)

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-// #include "math/vec.h"
+#include "math/vec.h"
 
 int main() {
   printf("Hello world !\n");
+  Vector vector;
+  createVector(&vector, 2);
   // struct vector * vector;
   // createVector(vector, 2);
-  // printf("Vector size : %d", vector->size);
+  printf("Vector size : %d\n", vector.size);
   return 0;
 }
--- src/math/vec.c
+++ src/math/vec.c
@@ -1,10 +1,10 @@
-// #include "vec.h"
-// #include <stdlib.h>
+#include "vec.h"
+#include <stdlib.h>
 
-// void createVector(Vector * vector, int size) {
-//     vector->size = size;
-//     vector->components = (float*)malloc(size * sizeof(float));
-//     for (short i = 0; i < size; i++) {
-//         vector->components[i] = 0.0f;
-//     }
-// }
+void createVector(Vector * vector, int size) {
+    vector->size = size;
+    vector->components = (float*)malloc(size * sizeof(float));
+    for (short i = 0; i < size; i++) {
+        vector->components[i] = 0.0f;
+    }
+}
